ce4123aabdb885766cabdb103730569f65b9c07c
chris <cseiter@gmail.com>
2021-03-21 18:49:02 -0400

    edit index.js with bot a for and while loop

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(recipients,event) {
+	let writtenCards = [];
+	for (let i = 0; i < recipients.length; i++) {
+		writtenCards.push(`Thank you, ${recipients[i]}, for the wonderful ${event} gift!`);
+	}
+	return writtenCards;
+}
+
+function countDown(starter) {
+	while (starter >= 0) {
+	console.log(starter);
+	starter -= 1;
+	}
+}
